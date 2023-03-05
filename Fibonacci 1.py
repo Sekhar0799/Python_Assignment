d498dc12717998a7132cddb674b7fa005c91b47b
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-03-05 12:48:54 +0530

    Update Fibonacci 1.py

--- Fibonacci 1.py
+++ Fibonacci 1.py
@@ -1,3 +1,9 @@
+# The Fibonacci Sequence is the series of numbers :
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+# Every next number is found by adding up the two numbers before it.
+# Expected Output : 1 1 2 3 5 8 13 21 34
+
+
 a = 0
 b = 1
 
@@ -7,4 +13,3 @@ while b <= 50:
     
 # output
 1 1 2 3 5 8 13 21 34 
-
