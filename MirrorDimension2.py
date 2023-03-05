7afcc8bf043bda73310915231eeca23ea12cf282
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-03-05 12:28:44 +0530

    Update MirrorDimension2.py

--- MirrorDimension2.py
+++ MirrorDimension2.py
@@ -1,3 +1,9 @@
+# Write a Python program that accepts a word from the user and reverse it.
+# Sample Test Case
+# Input : Edyoda
+# output: adoydE
+
+
 name = input("Enter the name")
 reverse_name=name[::-1]
 print(reverse_name)
