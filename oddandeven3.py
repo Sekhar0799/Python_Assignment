81f8b96c1b3538813f6426c20062e8c70b2589d4
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-03-05 12:50:41 +0530

    Update oddandeven3.py

--- oddandeven3.py
+++ oddandeven3.py
@@ -1,3 +1,9 @@
+# Write a Python program to count the number of even and odd numbers from a series of numbers.
+# Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+# Expected Output :
+# Number of even numbers : 4
+# Number of odd numbers : 5
+
 numbers = (1,2,3,4,5,6,7,8,9)
 even_count = 0
 odd_count = 0
@@ -8,7 +14,6 @@ for i in numbers:
         odd_count += 1
 print("Number of even numbers:",even_count)
 print("Number of odd numbers:",odd_count)
-
 
 # output
 
