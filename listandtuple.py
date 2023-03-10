8983785df189f3453f3dd97a5b96e8065e15197e
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-03-10 22:13:36 +0530

    Update listandtuple.py

--- listandtuple.py
+++ listandtuple.py
@@ -1,4 +1,11 @@
+# Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples
+# Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+
 
 sample_list1 =[(2,5),(1,2),(4,4),(2,3),(2,1)]
+# By using lambda inside sorted()
 sorted_list1=sorted(sample_list1,key=lambda x:x[-1])
 print(sorted_list1)
+
+# Output
+# [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
