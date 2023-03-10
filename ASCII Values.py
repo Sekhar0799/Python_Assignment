7d9c7eadce484e941976d4a9342d5822eaf23fdf
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-03-10 22:09:23 +0530

    Update ASCII Values.py

--- ASCII Values.py
+++ ASCII Values.py
@@ -1,12 +1,21 @@
+# Write a Python program to print a dictionary whose keys should be the alphabet from a-z and the value should be corresponding ASCII values
+
 # Method 1
 ascii_dict = {}
 for num in range(97, 123):
     ascii_dict[chr(num)] = num
 print(ascii_dict)
 
-# Method 2
+#Output
+# {'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 109, 
+# 'n': 110, 'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122}
 
+# Method 2
 ascii_dic1={}
 for char in range(ord('a'),ord('z')+1):
     ascii_dic1[chr(char)]= char
 print(ascii_dic1)
+
+#Output
+# {'a': 97, 'b': 98, 'c': 99, 'd': 100, 'e': 101, 'f': 102, 'g': 103, 'h': 104, 'i': 105, 'j': 106, 'k': 107, 'l': 108, 'm': 109, 
+# 'n': 110, 'o': 111, 'p': 112, 'q': 113, 'r': 114, 's': 115, 't': 116, 'u': 117, 'v': 118, 'w': 119, 'x': 120, 'y': 121, 'z': 122}
