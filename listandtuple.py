51410e2645e73497e3d2f00f0f119f1cee78904d
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-03-11 17:12:11 +0530

    Update listandtuple.py

--- listandtuple.py
+++ listandtuple.py
@@ -1,7 +1,28 @@
 # Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples
 # Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+#By using input user
 
+tup_list = []
+n = int(input("Enter the number of tuples:"))
+for i in range(n):
+    tup_input = input("Enter the tuple separated by comma:")
+    tup_val = tuple(map(int,tup_input.split(',')))
+    tup_list.append(tup_val)
+sorted_tup_list = sorted(tup_list,key=lambda x:x[-1])
+print(sorted_tup_list)
 
+#Output
+
+# Enter the number of tuples:5
+# Enter the tuple separated by comma:2,5
+# Enter the tuple separated by comma:1,2
+# Enter the tuple separated by comma:4,4
+# Enter the tuple separated by comma:2,3
+# Enter the tuple separated by comma:2,1
+
+# [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
+#Method 2
 sample_list1 =[(2,5),(1,2),(4,4),(2,3),(2,1)]
 # By using lambda inside sorted()
 sorted_list1=sorted(sample_list1,key=lambda x:x[-1])
