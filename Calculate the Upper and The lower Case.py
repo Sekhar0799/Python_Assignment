88fb1b653ea168886d946d1c586d71000eb43b2a
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-03-18 12:45:57 +0530

    Update Calculate the Upper and The lower Case.py

--- Calculate the Upper and The lower Case.py
+++ Calculate the Upper and The lower Case.py
@@ -1,3 +1,10 @@
+# Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
+# Sample String : 'The quick Brow Fox'
+# Expected Output :
+# No. of Upper case characters : 3
+# No. of Lower case Characters : 12
+
+
 def count_upper_lower():
     user_str1 = input("Enter a string: ")
     upper_count = 0
@@ -10,3 +17,8 @@ def count_upper_lower():
     print("No. of Upper case characters : ", upper_count)
     print("No. of Lower case characters : ", lower_count)
 count_upper_lower()
+
+# Output
+# Enter a string: The quick Brow Fox
+# No. of Upper case characters :  3
+# No. of Lower case characters :  12
