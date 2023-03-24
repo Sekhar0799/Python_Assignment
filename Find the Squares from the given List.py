04ba8ddc88559514016283b56d32c6eaec1ba0b6
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-03-24 21:55:20 +0530

    Update Find the Squares from the given List.py

--- Find the Squares from the given List.py
+++ Find the Squares from the given List.py
@@ -1,3 +1,9 @@
+# Write a Python program to square the elements of a list using map() function.
+# Sample List: [4, 5, 2, 9]
+# Square the elements of the list:
+# [16, 25, 4, 81]
+
+
 input_str = input("Enter a list of integers separated by space: ")
 input_list = list(map(int, input_str.strip().split()))
 def square(num):
@@ -5,3 +11,8 @@ def square(num):
 square_list = list(map(square, input_list))
 print("Square of list numbers:")
 print(square_list)
+
+#output
+# Enter a list of integers separated by space: 4 5 2 9
+# Square of list numbers:
+# [16, 25, 4, 81]
