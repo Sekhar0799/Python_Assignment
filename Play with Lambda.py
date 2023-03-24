8a92b08eaf470b892101d868e682e0acb78eb109
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-03-24 21:47:35 +0530

    Update Play with Lambda.py

--- Play with Lambda.py
+++ Play with Lambda.py
@@ -1,4 +1,12 @@
+# Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+# sample input: 10
+# sample output: 35
+
 add_25 = lambda num: num + 25
 num = int(input("Enter a number: "))
 result = add_25(num)
 print(f"The result of adding 25 to {num} is {result}")
+
+#output
+# Enter a number: 10
+# The result of adding 25 to 10 is 35
