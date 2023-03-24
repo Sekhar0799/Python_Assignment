abb50ce8ec978d8ec31a346eb725c199582ca026
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-03-24 21:51:51 +0530

    Update Find the way with Maps.py

--- Find the way with Maps.py
+++ Find the way with Maps.py
@@ -1,3 +1,8 @@
+# Write a Python program to triple all numbers of a given list of integers. Use Python map.
+# sample list: [1, 2, 3, 4, 5, 6, 7]
+# Triple of list numbers:
+# [3, 6, 9, 12, 15, 18, 21]
+
 input_str = input("Enter a list of integers separated by space: ")
 input_list = list(map(int, input_str.strip().split()))
 def triple(num):
@@ -5,3 +10,8 @@ def triple(num):
 triple_list = list(map(triple, input_list))
 print("Triple of list numbers:")
 print(triple_list)
+
+# output
+# Enter a list of integers separated by space: 1 2 3 4 5 6 7
+# Triple of list numbers:
+# [3, 6, 9, 12, 15, 18, 21]
