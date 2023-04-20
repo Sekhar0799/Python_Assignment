ac8e6fbcb59cd077e97379e20ff2f8a40298d796
Sekhar0799 <104270199+Sekhar0799@users.noreply.github.com>
2023-04-20 19:15:26 +0530

    Update Assignment DSA.py

--- Assignment DSA.py
+++ Assignment DSA.py
@@ -1,3 +1,5 @@
+# Q1. Write a program to find all pairs of an integer array whose sum is equal to a given number?
+
 def find_pairs(arr, target_sum):
     pairs = []
     for i in range(len(arr)):
@@ -9,6 +11,11 @@ def find_pairs(arr, target_sum):
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 target_sum = 10
 print(find_pairs(arr, target_sum))  
+
+# output
+[(1, 9), (2, 8), (3, 7), (4, 6)]
+
+# Q2. Write a program to reverse an array in place? In place means you cannot create a new array. You have to update the original array.
 
 def reverse_array(arr):
     start = 0
@@ -22,6 +29,10 @@ def reverse_array(arr):
 arr = [1, 2, 3, 4, 5, 6]
 print(reverse_array(arr)) 
 
+# Output
+[6, 5, 4, 3, 2, 1]
+
+# Q3. Write a program to check if two strings are a rotation of each other?
 def are_rotations(str1, str2):
     if len(str1) != len(str2):
         return False
@@ -34,6 +45,11 @@ def are_rotations(str1, str2):
 str1 = 'abcd'
 str2 = 'cdab'
 print(are_rotations(str1, str2))  
+
+#Output
+True
+
+# Q4. Write a program to print the first non-repeated character from a string?
 
 def first_non_repeated_character(string):
     char_count = {}
@@ -50,6 +66,11 @@ def first_non_repeated_character(string):
 string = "abacddbec"
 print(first_non_repeated_character(string)) 
 
+# Output
+e
+
+# Q5. Read about the Tower of Hanoi algorithm. Write a program to implement it.
+
 def tower_of_hanoi(n, from_rod, to_rod, aux_rod):
     if n == 1:
         print("Move disk 1 from rod", from_rod, "to rod", to_rod)
@@ -60,6 +81,18 @@ def tower_of_hanoi(n, from_rod, to_rod, aux_rod):
 
 n = 3
 tower_of_hanoi(n, 'A', 'C', 'B')
+
+# output
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+# Q6. Read about infix, prefix, and postfix expressions. Write a program to convert postfix to prefix expression.
 
 def postfix_to_prefix(expression):
     stack = []
@@ -77,6 +110,11 @@ def postfix_to_prefix(expression):
 expression = "AB+CD-*"
 print(postfix_to_prefix(expression))  
 
+# output
+*+AB-CD
+
+# Q7. Write a program to convert prefix expression to infix expression.
+
 def prefix_to_infix(expression):
     stack = []
     operators = set(['+', '-', '*', '/', '^'])
@@ -91,6 +129,11 @@ def prefix_to_infix(expression):
 
 expression = "-+AB*CD"
 print(prefix_to_infix(expression))  
+
+# output
+((A+B)-(C*D))
+
+# Q8. Write a program to check if all the brackets are closed in a given code snippet.
 
 def check_brackets(code):
     stack = []
@@ -110,7 +153,6 @@ def check_brackets(code):
         return False
     return True
 
-
 code_snippet = 'print("Hello, World!")'
 print(check_brackets(code_snippet)) 
 
@@ -119,6 +161,13 @@ print(check_brackets(code_snippet))
 
 code_snippet = 'for (i = 0; i < n; i++ { print(i) }'
 print(check_brackets(code_snippet))  
+
+# output
+True
+True
+False
+
+# Q9. Write a program to reverse a stack.
 
 def reverse_stack(stack):
     if len(stack) == 0:
@@ -134,11 +183,15 @@ def insert_at_bottom(stack, item):
     temp = stack.pop()
     insert_at_bottom(stack, item)
     stack.append(temp)
-
-
+    
 stack = [1, 2, 3, 4, 5]
 reverse_stack(stack)
 print(stack)  
+
+# Output
+[5, 4, 3, 2, 1]
+
+# Q10. Write a program to find the smallest number using a stack.
 
 class MinStack:
     def __init__(self):
@@ -173,3 +226,7 @@ print(stack.get_min())
 stack.pop()
 stack.pop()
 print(stack.get_min()) 
+
+# Output
+1
+3
